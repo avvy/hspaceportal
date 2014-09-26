9c226d0ad5542cfbad186998e4bd03993ae061df
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2014-09-26 07:51:11 +0300

    skills export script

--- data/skills.php
+++ data/skills.php
@@ -1,21 +1,50 @@
 <?php
 $fn = "skills.csv"; // input file name
 $hasHeader = true; // input file has header, we will skip first line
-$hasRoot = false;
 //
-$msg = "[error] file not found: ".$fn;
+function appendItem( &$r, $lvl, $item ) {
+	if ( $lvl ) {
+		$lvl--;
+		end( $r ); 
+		appendItem( $r[key($r)], $lvl, $item );
+	} else {
+		$r[$item] = array(); 
+	}
+}
+
+function neo4j( &$r, $parent, &$ndex ) {
+	foreach( $r as $rk => $rv ) {
+		$ndex++;
+		$name = "skill".$ndex;
+		echo "(".$name.":skill { id:'".$rk."'}),"."</br>";
+		if ( $parent ) {
+			echo "(".$parent.")-[:linked]->(".$name."),"."</br>";
+		}
+		neo4j( $rv, $name, $ndex );
+	}
+}
+
 $r = array();
 if ( ( $handle = fopen( $fn, "r" ) ) !== FALSE ) {
-	$msg = "ok";
 	$header = true;
 	while ( ( $data = fgetcsv( $handle, 1000, "," ) ) !== FALSE ) {
 		if ( $header and $hasHeader ) {
 			$header = false;
 		} else {
-			print_r( $data );
-			break;
+			$lvl = 0;
+			foreach( $data as $dv ) {
+				$v = trim( $dv );
+				if ( $v ) {
+					appendItem( $r, $lvl, $v );
+					break;
+				}
+				$lvl++;
+			}
 		}
 	}
 }
-echo $msg;
+//echo json_encode( $r );
+$index = 0;
+echo "CREATE ";
+neo4j( $r, "", $index );
 ?>
