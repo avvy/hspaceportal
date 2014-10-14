7b36014aedb7e66515f04d22e3754ff8182ef2e7
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2014-10-14 16:13:03 +0300

    hard/soft skills

--- data/export.php
+++ data/export.php
@@ -2,7 +2,8 @@
 header('Content-Type: application/json');
 include './../util.php';
 //
-neo4j2json( "skill.json", array( "tag", "skill" ) );
-neo4j2json( "interest.json", array( "tag", "interest" ) );
-neo4j2json( "contact.json", array( "contact" ) );
+neo4j2json( "hard-skill.json", array( "tag", "skill", "hard" ) );
+// neo4j2json( "soft-skill.json", array( "tag", "skill", "soft" ) );
+//neo4j2json( "interest.json", array( "tag", "interest" ) );
+//neo4j2json( "contact.json", array( "contact" ) );
 ?>
